a7c16a57b1a72be4ab0da1ee0d404dfbb44dbdc5
yinm <yinm.cminor9@gmail.com>
2018-12-15 08:34:01 +0900

    Add config

---
+++ 01_getting_started/app/db/db-config.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize')
+
+const dbConfig = new Sequelize('company', 'yinm', '', {
+  host: 'localhost',
+  dialect: 'postgres',
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+})
+
+module.exports = dbConfig
