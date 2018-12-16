da7a218622ea8054abcbb947f1d50e1303de3fbc
yinm <yinm.cminor9@gmail.com>
2018-12-16 20:00:35 +0900

    Add

--- 06_sequelize-typescript-example/lib/app.ts
+++ 06_sequelize-typescript-example/lib/app.ts
@@ -6,13 +6,10 @@ import {actors} from './routes/actors';
 
 export const app = express();
 
-// middleware for parsing application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 
-// middleware for json body parsing
 app.use(bodyParser.json({limit: '5mb'}));
 
-// enable corse for all origins
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Expose-Headers", "x-total-count");
